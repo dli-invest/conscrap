486ddd136375cde8bbb48387920d9b1231db7e22
David Li <davidli012345@gmail.com>
2021-01-02 02:27:21 +0000

    adding documentation

--- ConScrap/Render.cs
+++ ConScrap/Render.cs
@@ -2,8 +2,14 @@
 using Scriban;
 namespace ConScrap
 {
+    /// <summary>
+    /// Simple Utility functions to Render Scriban Templates and load Template Files
+    /// </summary>
     public static class Render
     {
+        /// <summary>
+        /// Parse Template
+        /// </summary>
         public static Scriban.Template ParseTemplate(string templateStr) 
         {
             if (templateStr is null)
