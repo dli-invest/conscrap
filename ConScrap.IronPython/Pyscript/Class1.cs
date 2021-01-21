7ead023821dd97e3a8e51df949ba6373b873e0e7
David Li <davidli012345@gmail.com>
2021-01-21 06:01:45 +0000

    adding features to attempt to pyscript

---
+++ ConScrap.IronPython/Pyscript/Class1.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+
+// namespace Lib
+// {
+//     public class Class1
+//     {
+//     }
+// }
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+// to install dependencies
+namespace RunPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            scope.SetVariable("test", "2 days ago");
+            engine.ExecuteFile(@"parse_date.py", scope);
+            Console.WriteLine(scope);
+            // var date = scope.getDate("2 days ago");
+            // Console.WriteLine(date);
+        }
+    }
+}
