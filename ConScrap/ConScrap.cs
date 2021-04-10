0e694c62c24baf334e021bb476a768ad867e8a0b
David Li <davidli012345@gmail.com>
2021-04-10 23:25:56 +0000

    switching tests to CMC.CN

--- ConScrap/ConScrap.cs
+++ ConScrap/ConScrap.cs
@@ -16,7 +16,7 @@ namespace ConScrap
     /// \todo get list of stocks
     public class ConScrap
     {
-        public static List<Types.YahooComment> GetYahooComments(string ticker = "PKK.CN")
+        public static List<Types.YahooComment> GetYahooComments(string ticker = "CMC.CN")
         {
             string readText = Browser.GetAllEntries(ticker);
             var htmlDoc = Parse.MkHtmlDoc(readText);
