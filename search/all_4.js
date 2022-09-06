fe83476e228300aaef7845d69277f3c939875f70
FriendlyUser <FriendlyUser@users.noreply.github.com>
2022-09-06 04:50:58 +0000

    deploy: 4a5e4bfc8d732f96c64e997a821ad3622ec2f2c9

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['fetchconfig_0',['FetchConfig',['../classConScrap_1_1Types_1_1FetchConfig.html',1,'ConScrap::Types']]],
-  ['fetchstocks_1',['FetchStocks',['../classConScrap_1_1ConScrap.html#ab0eea27e294ab0e87b572102bb1fbf07',1,'ConScrap::ConScrap']]]
+  ['fetchstocks_1',['FetchStocks',['../classConScrap_1_1ConScrap.html#a8aff872cfff6c1e457e1ed30266fa98c',1,'ConScrap::ConScrap']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fetchstocks_0',['FetchStocks',['../classConScrap_1_1ConScrap.html#ab0eea27e294ab0e87b572102bb1fbf07',1,'ConScrap::ConScrap']]]
+  ['fetchstocks_0',['FetchStocks',['../classConScrap_1_1ConScrap.html#a8aff872cfff6c1e457e1ed30266fa98c',1,'ConScrap::ConScrap']]]
 ];
